27312e0982d46e71ca055af2593affee5c598637
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-02-28 11:08:43 -0600

    Add Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 
+import { ClerkProvider } from '@clerk/nextjs';
 import { IBM_Plex_Sans } from 'next/font/google';
 import type { Metadata } from 'next';
 import { cn } from '@/lib/utils';
@@ -21,11 +22,20 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			{/* Establece las clases CSS para el elemento <body>. Está utilizando una combinación de clases, donde 'font-IBMPlex' indica el uso de la fuente IBM Plex, 'antialiased' sugiere el uso de suavizado de bordes para mejorar la legibilidad de las fuentes, y IBMPlex.variable indica el uso de variables de fuente específicas de IBM Plex. */}
-			<body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>
-				{children}
-			</body>
-		</html>
+		<ClerkProvider
+			appearance={{
+				variables: { colorPrimary: '#624cf5' },
+			}}>
+			<html lang="en">
+				{/* Establece las clases CSS para el elemento <body>. Está utilizando una combinación de clases, donde 'font-IBMPlex' indica el uso de la fuente IBM Plex, 'antialiased' sugiere el uso de suavizado de bordes para mejorar la legibilidad de las fuentes, y IBMPlex.variable indica el uso de variables de fuente específicas de IBM Plex. */}
+				<body
+					className={cn(
+						'font-IBMPlex antialiased',
+						IBMPlex.variable
+					)}>
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
